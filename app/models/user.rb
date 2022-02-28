c571883e78f895757b3c413deb423b2a4d4f0b2a
Alex Krueger <alexckrueger@gmail.com>
2022-02-28 09:48:16 -0600

    adds businesses custom association for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   has_many :favorites
   # custom - has_many :businesses, through: :favorites
 
+  def businesses
+    favorite_businesses = Favorite.where(user_id: id)
+    favorites = []
+    favorite_businesses.each do |business|
+      favorites << HTTP.auth("Bearer " + Rails.application.credentials.yelp_api_key).get("https://api.yelp.com/v3/businesses/#{business.business_id}").parse(:json)
+    end
+    favorites
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@
 
 # review = Review.new(user_id: 1, business_id: "8vFJH_paXsMocmEO_KAa3w", overall_rating: 5, veggie_options_rating: 4, veggie_friendly_menu_rating: 3, recommended_dishes: "All the pizzas", comment: "Wowiee it's Lou's! They're iconic!")
 # review.save
+
+# Favorite.create(user_id: 1, business_id: "8vFJH_paXsMocmEO_KAa3w")
+# Favorite.create(user_id: 1, business_id: "DXwSYgiXqIVNdO9dazel6w")
+# Favorite.create(user_id: 1, business_id: "K_rdigDLXDYoyCjerZLQsg")
