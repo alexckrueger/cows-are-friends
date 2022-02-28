54fbc3659040b2709a337c3fcc4ed06110b3963e
Alex Krueger <alexckrueger@gmail.com>
2022-02-28 09:51:50 -0600

    corrects association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+
   has_many :reviews
   has_many :favorites
-  # custom - has_many :businesses, through: :favorites
 
   def businesses
-    favorite_businesses = Favorite.where(user_id: id)
-    favorites = []
-    favorite_businesses.each do |business|
-      favorites << HTTP.auth("Bearer " + Rails.application.credentials.yelp_api_key).get("https://api.yelp.com/v3/businesses/#{business.business_id}").parse(:json)
+    favorite_businesses = []
+    favorites.each do |business|
+      favorite_businesses << HTTP.auth("Bearer " + Rails.application.credentials.yelp_api_key).get("https://api.yelp.com/v3/businesses/#{business.business_id}").parse(:json)
     end
-    favorites
+    favorite_businesses
   end
 
 end
