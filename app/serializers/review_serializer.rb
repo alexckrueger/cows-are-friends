35591e99a1beb17dd1311016e988ae1fbeb264ea
Alex Krueger <alexckrueger@gmail.com>
2022-02-28 10:09:41 -0600

    adds serializers for user and review

---
+++ app/serializers/review_serializer.rb
@@ -0,0 +1,9 @@
+class ReviewSerializer < ActiveModel::Serializer
+  attributes :id, :user_id, :business_id, :overall_rating, :veggie_options_rating, :veggie_friendly_menu_rating, :recommended_dishes, :comment, :image_url
+
+  attribute :owner, if: :current_user
+
+  def owner
+    current_user.id == object.user_id
+  end
+end
