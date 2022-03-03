3125ce586949eb9844b3227f2eecfef532e38f36
Alex Krueger <alexckrueger@gmail.com>
2022-03-02 19:55:38 -0600

    adds validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
+  validates :overall_rating, numericality: { in: 1..5 }
+  validates :veggie_options_rating, numericality: { in: 1..5 }
+  validates :veggie_friendly_menu_rating, numericality: { in: 1..5 }
+  validates :comment, length: { in: 8..200 }
 
   def business
     HTTP.auth("Bearer " + Rails.application.credentials.yelp_api_key).get("https://api.yelp.com/v3/businesses/#{business_id}").parse(:json)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :name, length: { minimum: 2 }
 
   has_many :reviews
   has_many :favorites
